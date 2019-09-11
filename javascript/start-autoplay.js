fa9c2d5f1829f50d471a22ab89275a601a9cbb37
Haruki Saito <haruki.saito@artcom.de>
2019-09-11 15:33:45 +0200

    cleaned

--- javascript/start-autoplay.js
+++ javascript/start-autoplay.js
@@ -1,10 +1,6 @@
-console.log("HELLO WORLD");
-
 $(function() {
   var screenWidth = $(window).width();
   if (screenWidth >= 800) {
     $('.video').attr('autoplay', 'autoplay');
   }
 });
-
-console.log("HELLO WORLD");
