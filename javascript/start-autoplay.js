87167de6bf99de19e10773283ef6dd543157cf8b
Haruki Saito <haruki.saito@artcom.de>
2019-09-11 14:55:35 +0200

    added autoplay only for desktop

---
+++ javascript/start-autoplay.js
@@ -0,0 +1,10 @@
+console.log("HELLO WORLD");
+
+$(function() {
+  var screenWidth = $(window).width();
+  if (screenWidth >= 800) {
+    $('.video').attr('autoplay', 'autoplay');
+  }
+});
+
+console.log("HELLO WORLD");
